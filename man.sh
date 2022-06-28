3647e745623d6906588a3af0fbd7451a7d476a13
megamandev <97738977+megamandev@users.noreply.github.com>
2022-06-28 17:15:45 -0300

    Update man.sh

--- man.sh
+++ man.sh
@@ -38,8 +38,8 @@ sudo service xrdp start
 clear
 echo "=======================100%"
 echo "===================================="
-echo "Username : aank"
-echo "Password : LinggaHosting"
+echo "Username : youruser"
+echo "Password : yourpass"
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
